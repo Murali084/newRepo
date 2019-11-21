7bf2cf89e31cd2882c36ab62c97642fd4f3bf3d0
Murali084 <dharani084@gmail.com>
2019-11-21 08:21:55 +0530

    new code

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -5,6 +5,8 @@ public class AxisBank extends BankInfo{
 	@Override
 	public void deposit() {
 		System.out.println("Fixed deposit without premature withdrawal");
+		System.out.println("Fixed deposit without premature withdrawal");
+
 	}
 
 	public static void main(String[] args) {
