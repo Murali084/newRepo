c8ce635387f59e7b3fb376fe9e05f7b5e383a411
Murali084 <dharani084@gmail.com>
2019-11-21 08:15:17 +0530

    Bank

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+	
+	@Override
+	public void deposit() {
+		System.out.println("Fixed deposit without premature withdrawal");
+	}
+
+	public static void main(String[] args) {
+		AxisBank a = new AxisBank();
+		a.deposit();
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,22 @@
+package org.bank;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("Saving account");
+	}
+
+	public void fixed() {
+		System.out.println("Fixed percent");
+	}
+
+	public void deposit() {
+		System.out.println("Fixed deposit with Montly income option");
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
